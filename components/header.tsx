185ce2fd1745e96c15e3eff065e68062f5cf3197
linas <linas.satkauskas000@gmail.com>
2024-03-03 19:44:04 +0200

    Pakeistas header

--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <header className="border-b border-blue-400 p-1 mb-5">
       <div className="flex gap-x-2">
         <AcademicCapIcon className="h-8 w-8 stroke-orange-700" />
-        <div className="text-2x1 text-orange-700 font-bold">AAS</div>
+        <div className="text-2x1 text-orange-700 font-bold">LAS</div>
       </div>
     </header>
   )
