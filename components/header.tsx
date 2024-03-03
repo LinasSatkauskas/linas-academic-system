375e9265abd07b8fd61b290a920c6250d3b633d0
linas <linas.satkauskas000@gmail.com>
2024-03-03 19:40:44 +0200

    6 Uzdavinys, baigtas

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+import { AcademicCapIcon } from "@heroicons/react/24/outline"
+
+export function Header() {
+  return (
+    <header className="border-b border-blue-400 p-1 mb-5">
+      <div className="flex gap-x-2">
+        <AcademicCapIcon className="h-8 w-8 stroke-orange-700" />
+        <div className="text-2x1 text-orange-700 font-bold">AAS</div>
+      </div>
+    </header>
+  )
+}
