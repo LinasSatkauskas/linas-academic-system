c7404371ce80eeadf2e629be1ba5736ef07e671f
Linas Šatkauskas <linas.satkauskas000@gmail.com>
2024-03-20 14:32:26 +0200

    perdarytas page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="grid grid-cols-4">
+      <div> Studentai</div>
+      <div> Tvarkaraščiai</div>
+      <div> Dėstytojai</div>
+      <div> Informacija</div>
+    </div>
+  )
+}
