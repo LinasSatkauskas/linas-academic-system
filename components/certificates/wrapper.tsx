a5e0a5835c6a9d03e22aed949e4be0346d1f27a3
Linas Šatkauskas <linas.satkauskas000@gmail.com>
2024-04-10 14:52:31 +0300

    Sukurtas wrapper.tsx

---
+++ components/certificates/wrapper.tsx
@@ -0,0 +1,40 @@
+"use client"
+import { ICertType, ICertificate } from "@/types/certificates"
+import { Form } from "./form"
+import { CertList } from "./list"
+import { useEffect, useState } from "react"
+import { getApi } from "@/utils/serverApi"
+
+type IProps = { certTypes: ICertType[] }
+
+export function Wrapper(props: IProps) {
+  const { certTypes } = props
+  const [editCert, setEditCert] = useState<ICertificate | undefined>()
+  const [certificates, setCertificates] = useState<ICertificate[]>([])
+
+  const getCertFromApi = () => {
+    getApi<ICertificate[]>("/api/certificates").then((res: ICertificate[]) => {
+      setCertificates(res)
+    })
+  }
+
+  useEffect(() => {
+    getCertFromApi()
+  }, [])
+
+  return (
+    <div className="grid gap-y-8">
+      <Form
+        certTypes={certTypes}
+        getCertFromApi={getCertFromApi}
+        setEditCert={setEditCert}
+        editCert={editCert}
+      />
+      <CertList
+        certTypes={certTypes}
+        certificates={certificates}
+        setEditCert={setEditCert}
+      />
+    </div>
+  )
+}
