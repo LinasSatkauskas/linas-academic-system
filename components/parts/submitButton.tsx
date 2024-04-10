277e0456bb896a4aa7b0d8ddc7dea2d51b895421
Linas Šatkauskas <linas.satkauskas000@gmail.com>
2024-04-10 11:29:58 +0300

    Įdėtas "submitButton"

---
+++ components/parts/submitButton.tsx
@@ -0,0 +1,14 @@
+import { useFormStatus } from "react-dom"
+
+export function SubmitButton() {
+  const { pending } = useFormStatus()
+  return (
+    <button
+      type="submit"
+      disabled={pending}
+      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-3000 font-medium rounded-lg text-sm px-5 py-1 me-2 mb-2"
+    >
+      Siųsti
+    </button>
+  )
+}
