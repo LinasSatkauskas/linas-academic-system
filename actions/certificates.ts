837af10254f06cda316564567034ce7f42fb7b32
Linas Šatkauskas <linas.satkauskas000@gmail.com>
2024-04-10 12:35:27 +0300

    Sukūrtas certificates.ts

---
+++ actions/certificates.ts
@@ -0,0 +1,36 @@
+"use server"
+import { IState } from "@/types/shared.t"
+import { postApi, putApi } from "@/utils/serverApi"
+import { z } from "zod"
+
+export async function createCertificates(
+  prevState: IState,
+  formData: FormData
+): Promise<IState> {
+  const schema = z.object({
+    id: z.coerce.number().optional(),
+    typeId: z.string().min(2),
+  })
+  const rawFormData = {
+    id: formData.get("id"),
+    typeId: formData.get("typeId"),
+    company: formData.get("company"),
+  }
+
+  const parse = schema.safeParse(rawFormData)
+
+  if (!parse.success) {
+    return {
+      errors: parse.error.flatten().fieldErrors,
+      message: "Blogai užpuldyti laukeliai!",
+      isSaved: false,
+    }
+  }
+  const dto = parse.data
+  if (!dto?.id) {
+    await postApi("/api/certificates", dto)
+    return { message: "Duomenys sėkmingai išsiųsti", isSaved: true }
+  }
+  await putApi("/api/certificates/${dto.id}", dto)
+  return { message: "Atnaujinti duomenys sėkmingai", isSaved: true }
+}
