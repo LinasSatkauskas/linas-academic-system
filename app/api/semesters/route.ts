8cfc1627a6f3fd001bf7ee7e833a543ce9754635
Linas Šatkauskas <linas.satkauskas000@gmail.com>
2024-04-17 12:40:04 +0300

    Ištrintas trečias semestras

--- app/api/semesters/route.ts
+++ app/api/semesters/route.ts
@@ -4,7 +4,6 @@ import { type NextRequest } from "next/server"
 const semestersDb: ISemester[] = [
   { id: 1, name: "Pirmas" },
   { id: 2, name: "Antras" },
-  { id: 3, name: "Trecias" },
 ]
 export function GET(request: NextRequest) {
   return Response.json(semestersDb)
