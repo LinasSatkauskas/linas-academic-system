0b68daa6fcba45d43014ccb7aae5c2e8d81646b5
Linas Satkauskas <linas.satkauskas000@gmail.com>
2024-05-20 12:00:42 +0300

    1 uzdavinys baigtas

---
+++ models/certType.model.ts
@@ -0,0 +1,25 @@
+import { model, models, Schema } from "mongoose"
+
+export interface ICertType {
+  id: String
+  title: String
+}
+
+const CertTypeSchema = new Schema<ICertType>(
+  {
+    title: String,
+  },
+  {
+    timestamps: false,
+    collection: "cert_types",
+    strict: true,
+    toJSON: {
+      versionKey: false,
+      virtuals: true,
+      transform: (_, ret) => {
+        ret.id = ret.id
+        delete ret.id
+      },
+    },
+  }
+)
