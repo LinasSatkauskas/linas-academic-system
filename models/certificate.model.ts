6bd951ef9314f98f06ebef685c98be40a550e1f4
Linas <linas.satkauskas000@gmail.com>
2024-05-21 15:37:55 +0300

    sutvarkyti id

--- models/certificate.model.ts
+++ models/certificate.model.ts
@@ -1,9 +1,9 @@
 import { model, models, Schema } from "mongoose"
 
 export interface ICertificate {
-  id?: String
-  typeId: String
-  company: String
+  id?: string
+  typeId: string
+  company: string
   isCreated?: boolean
 }
 
