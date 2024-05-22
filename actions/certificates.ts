2b29f4876aa939c26791960cd2621e3e2c5b4678
Linas <linas.satkauskas000@gmail.com>
2024-05-22 11:08:10 +0300

    keistas certifikatu failas

--- actions/certificates.ts
+++ actions/certificates.ts
@@ -13,8 +13,7 @@ export async function createCertificates(
     company: z.string().min(2),
   })
   const rawFormData = {
-    id: formData.get("id"),
-    typeId: formData.get("typeId"),
+    id: formData.has("id") ? formData.get("Id") : undefined,
     company: formData.get("company"),
   }
 
